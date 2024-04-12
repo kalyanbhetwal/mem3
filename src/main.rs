1e91a2eb5e6e647833300ea2902d625b0b32956b
kalyanbhetwal <kalyanbtl@gmail.com>
2024-04-12 17:51:49 -0600

    cleanup codes

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ use volatile::Volatile;
 use core::sync::atomic::{compiler_fence, Ordering};
 use stm32f3xx_hal_v2::hal::blocking::rng::Read;
 
+mod my_flash;
+use my_flash::{unlock, wait_ready, clear_error_flags, erase_page, write_to_flash};
+
 static mut x:u8 = 1;
 static mut y:u8 = 3;
 static mut z:u8 = 2;
@@ -66,108 +69,6 @@ fn read_data(i2c: &mut hal::i2c::I2c<pac::I2C1, (stm32f3xx_hal_v2::gpio::gpiob::
     i2c.write_read(address, &memory_address_bytes, data).unwrap();
 }
 
-
-fn unlock(flash: &mut FLASH) ->bool{
-
-    if flash.cr.read().lock().bit_is_clear(){
-        return true;
-    }
-
-    flash.keyr.write(|w| unsafe { w.bits(UNLOCK_KEY1) });
-    flash.keyr.write(|w| unsafe { w.bits(UNLOCK_KEY2) });
-
-    if flash.cr.read().lock().bit_is_clear() {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-fn wait_ready(flash: &FLASH) {
-    while flash.sr.read().bsy().bit() {}
-}
-
-fn clear_error_flags(flash: &FLASH) {
-    if flash.sr.read().wrprterr().bit_is_set() {
-        flash.sr.modify(|_, w| w.wrprterr().set_bit());
-    }
-    if flash.sr.read().pgerr().bit_is_set() {
-        flash.sr.modify(|_, w| w.pgerr().set_bit());
-    }
-}
-
-fn erase_page(flash: &mut FLASH, page: u32){
-
-    // 1. Check that no Flash memory operation is ongoing by checking the BSY bit in the Flash
-    // status register (FLASH_SR).
-    if flash.sr.read().bsy().bit_is_set() {
-        hprintln!("Flash is busy.");
-     }
-
-    // 2. Check and clear all error programming flags due to a previous programming. If not,
-     // PGSERR is set.
-    clear_error_flags(&flash);
-
-    // 3. Set the PER bit and select the page you wish to erase (PNB). For dual bank variants:
-     //  - with the associated bank(BKER) in the Flash control register (FLASH_CR).
-
-     flash.cr.modify(|_, w| w.per().set_bit());
-
-     // Program the FLASH_CR register
-     // regs.ar.modify(|_, w| w.far().bits(page as u8));
-     flash.ar.write(|w| unsafe { w.bits(page as u32) });
-
-
-     // 4. Set the STRT bit in the FLASH_CR register.
-     flash.cr.modify(|_, w| w.strt().set_bit());
-
-    // 5. Wait for the BSY bit to be cleared in the FLASH_SR register.
-      
-    while flash.sr.read().bsy().bit_is_set() {}
-
-    // 6. lock the flash
-    while flash.sr.read().bsy().bit_is_set() {}
-    flash.cr.modify(|_, w| w.lock().set_bit());
-
-}
-
-fn write_to_flash(flash: &mut FLASH, addr: u32, data: u32) {
-        unlock(flash);
-
-        // 1. Check that no Flash memory operation is ongoing by checking the BSY bit in the Flash
-        if flash.sr.read().bsy().bit_is_set() {
-            hprintln!("Flash is busy.");
-        }
-         
-        clear_error_flags(&flash);
-       // 2. Set the PG bit in the Flash control register (FLASH_CR).
-       flash.cr.modify(|_, w| w.pg().set_bit());
-
-
-        // 3. Perform the data write (half-word) at the desired address.
-        unsafe{
-                ptr::write_volatile(addr as *mut u16, data as u16);
-                ptr::write_volatile((addr as usize + 2) as * mut u16, (data.wrapping_shr(16)) as u16);
-        }
-
-        // 4. Wait for the BSY bit to be cleared in the FLASH_SR register.
-
-        while flash.sr.read().bsy().bit_is_set() {}
-        // 5. lock the flash
-        flash.cr.modify(|_, w| w.lock().set_bit());
-        
-
-        // 6. Check that EOP flag is set in the FLASH_SR register (meaning that the programming
-        // operation has succeed), and clear it by software.
-        if flash.sr.read().eop().bit_is_set() {
-            flash.sr.modify(|_, w| w.eop().set_bit()); // Clear
-        }
-
-         // 6. Clear the PG bit in the FLASH_CR register if there no more programming request
-        // anymore.
-        flash.cr.modify(|_, w| w.pg().clear_bit());
-
-}
 
 #[cfg(debug_assertions)]
 fn checkpoint(){
@@ -608,7 +509,9 @@ fn checkpoint_globals<T>(i2c: &mut hal::i2c::I2c<pac::I2C1, (stm32f3xx_hal_v2::g
        let byte_ptr = adrs as *const u8;
        buff[i+2] =  *byte_ptr.add(i);
     }
+
     i2c.write(FRAM_ADDRESS, &buff);
+
     back_up_address= back_up_address +len as u16;
     }
 }
@@ -627,6 +530,7 @@ pub extern "C" fn main() -> ! {
     unsafe{
         let c = y+z;
         //checkpoint_variables(&mut y, &mut z); // y and z are the exclusive may write variables
+        //inline
         checkpoint_globals(&mut i2c, &y as *const u8, mem::size_of_val(&y));
         checkpoint_globals(&mut i2c, &z as *const u8, mem::size_of_val(&z));
         checkpoint();
